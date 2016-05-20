9d26bba8952238e86db1b72b244543937fe1af17
ikalvari <ikalvari@ebi.ac.uk>
2016-05-20 18:05:11 +0100

    clan & motif xml builders implemented - optimizations required

--- rfam_search/rfam_xml_dumper.py
+++ rfam_search/rfam_xml_dumper.py
@@ -17,15 +17,20 @@ import xml.etree.ElementTree as ET
 import datetime
 import string
 from xml.dom import minidom
-from config import rfam_search as rs_conf
+from config import rfam_search as rs
 from utils import RfamDB  # will load those from rfam-public
 
 # ----------------------------------------------------------------------------
 
 # this should be able to dump a single family, clan, motif or the entire db..
+# entry type applies for all families
+# entry_acc for single entry export
+# need a flag for all or single entry export
 
 
-def xml4db_dumper(outdir):
+def xml4db_dumper(entry_type, entry_acc, outdir):
+
+    # params entry_type,
     '''
         exports query results into EB-eye's XML4dbDUMP format
         outdir: Destination directory
@@ -34,21 +39,34 @@ def xml4db_dumper(outdir):
         and according to that dump the xml file
     '''
 
+    entry_type = entry_type[0].capitalize()
+
     # EB_eye_search fixed tags
     db_xml = ET.Element("database")
-    ET.SubElement(db_xml, "name").text = rs_conf.DB_NAME
-    ET.SubElement(db_xml, "description").text = rs_conf.DB_DESC
-    ET.SubElement(db_xml, "release").text = rs_conf.DB_RELEASE
-    ET.SubElement(db_xml, "release_date").text = rs_conf.DB_REL_DATE
+    ET.SubElement(db_xml, "name").text = rs.DB_NAME
+    ET.SubElement(db_xml, "description").text = rs.DB_DESC
+    ET.SubElement(db_xml, "release").text = rs.DB_RELEASE
+    ET.SubElement(db_xml, "release_date").text = rs.DB_REL_DATE
+
+    # need to add a parameter for this...
+    ET.SubElement(db_xml, "entry_count").text = '1'
 
     entries = ET.SubElement(db_xml, "entries")
 
     # call family xml builder to add a new family to the xml tree
-    family_xml_builder(entries, rfam_acc='RF02560')
+    if (entry_type == rs.FAMILY):
+        family_xml_builder(entries, rfam_acc=entry_acc)
 
+    elif (entry_type == rs.CLAN):
+        clan_xml_builder(entries, clan_acc=entry_acc)
+
+    elif (entry_type == rs.MOTIF):
+        motif_xml_builder(entries, motif_acc=entry_acc)
+
+    # export xml tree
     tree = ET.ElementTree(db_xml)
 
-    fp_out = open(os.path.join(outdir, "xml4dbsample.xml"), 'w')
+    fp_out = open(os.path.join(outdir, entry_acc + ".xml"), 'w')
 
     db_str = ET.tostring(db_xml, 'utf-8')
     db_str_reformated = minidom.parseString(db_str)
@@ -68,20 +86,18 @@ def family_xml_builder(entries, rfam_acc=None):
         rfam_acc: A specific Rfam family accession
     '''
 
-    cnx = RfamDB.connect()
-    cursor = cnx.cursor(dictionary=True)
+    entry_type = 'Family'
 
     cross_refs = {}
 
     # fetch family fields
-    cursor.execute(rs_conf.FAM_QUERY % rfam_acc)
-    fam_fields = cursor.fetchall()[0]
+    fam_fields = fetch_entry_fields(rfam_acc, rs.FAMILY)
 
     # fetch family specific ncbi_ids
-    ncbi_ids = fetch_value_list(rfam_acc, rs_conf.NCBI_IDs_QUERY)
+    ncbi_ids = fetch_value_list(rfam_acc, rs.NCBI_IDs_QUERY)
 
     # fetch family specific ncbi_ids
-    pdb_ids = fetch_value_list(rfam_acc, rs_conf.PDB_IDs_QUERY)
+    pdb_ids = fetch_value_list(rfam_acc, rs.PDB_IDs_QUERY)
 
     # get pubmed ids
     pmids = get_value_list(fam_fields["pmids"], ',')
@@ -89,9 +105,10 @@ def family_xml_builder(entries, rfam_acc=None):
     # get dbxrefs (GO, SO)
     dbxrefs = get_value_list(fam_fields["dbxrefs"], ',')
 
-    clan = fetch_value(rs_conf.FAM_CLAN, rfam_acc)
+    # get associated clan
+    clan = fetch_value(rs.FAM_CLAN, rfam_acc)
 
-    # need a function here to split dbxrefs
+    # need a function here to split dbxrefs in a pretty way
     go_ids = filter(lambda x: x.find("GO") != -1, dbxrefs)
     so_ids = filter(lambda x: x.find("SO") != -1, dbxrefs)
 
@@ -101,14 +118,15 @@ def family_xml_builder(entries, rfam_acc=None):
     cross_refs["PUBMED"] = pmids
     cross_refs["GO"] = go_ids
     cross_refs["SO"] = so_ids
-    if clan != None:
+
+    if clan is not None:
         cross_refs["RFAM"] = [clan]
 
     # add a new family entry to the xml tree
     entry = ET.SubElement(entries, "entry", id=rfam_acc)
 
     # entry name
-    ET.SubElement(entry, "name").text = str(fam_fields["rfam_id"])
+    ET.SubElement(entry, "name").text = str(fam_fields["name"])
 
     # entry description
     ET.SubElement(entry, "description").text = str(fam_fields["description"])
@@ -127,10 +145,89 @@ def family_xml_builder(entries, rfam_acc=None):
 
     # expand xml tree with additional fields
     build_additional_fields(
-        entry, fam_fields, len(pdb_ids), entry_type='Family')
+        entry, fam_fields, len(pdb_ids), entry_type=entry_type)
 
-    cursor.close()
-    cnx.disconnect()
+
+# ----------------------------------------------------------------------------
+
+
+def clan_xml_builder(entries, clan_acc=None):
+    '''
+        Expands the Xml4dbDumper object by adding a new clan entry.
+
+        entries: The xml entries node to be expanded
+        clan_acc: A specific Rfam clan accession
+
+    '''
+
+    entry_type = 'Clan'
+
+    cross_ref_dict = {}
+
+    # fetch clan fields
+    clan_fields = fetch_entry_fields(clan_acc, rs.CLAN)
+
+    # add a new clan entry to the xml tree
+    entry = ET.SubElement(entries, "entry", id=clan_acc)
+
+    ET.SubElement(entry, "name").text = clan_fields["name"]
+    ET.SubElement(entry, "description").text = clan_fields["description"]
+
+    # entry dates - common to motifs and clans
+    dates = ET.SubElement(entry, "dates")
+
+    created = clan_fields["created"].date().strftime("%d %b %Y")
+    updated = clan_fields["updated"].date().strftime("%d %b %Y")
+
+    ET.SubElement(dates, "date", value=created, type="created")
+    ET.SubElement(dates, "date", value=updated, type="updated")
+
+    # clan cross references
+    clan_fams = fetch_value_list(clan_acc, rs.CLAN_FAMS)
+    cross_ref_dict["RFAM"] = clan_fams
+    build_cross_references(entry, cross_ref_dict)
+
+    # clan additional fields
+    build_additional_fields(
+        entry, clan_fields, clan_fields['num_families'], entry_type=entry_type)
+
+# ----------------------------------------------------------------------------
+
+
+def motif_xml_builder(entries, motif_acc=None):
+    '''
+        Expands the Xml4dbDump with a Motif entry.
+    '''
+
+    entry_type = 'Motif'
+
+    cross_ref_dict = {}
+
+    # fetch clan fields
+    motif_fields = fetch_entry_fields(motif_acc, rs.MOTIF)
+
+    # add a new clan entry to the xml tree
+    entry = ET.SubElement(entries, "entry", id=motif_acc)
+
+    ET.SubElement(entry, "name").text = motif_fields["name"]
+    ET.SubElement(entry, "description").text = motif_fields["description"]
+
+    # entry dates - common to motifs and clans
+    dates = ET.SubElement(entry, "dates")
+
+    created = motif_fields["created"].date().strftime("%d %b %Y")
+    updated = motif_fields["updated"].date().strftime("%d %b %Y")
+
+    ET.SubElement(dates, "date", value=created, type="created")
+    ET.SubElement(dates, "date", value=updated, type="updated")
+
+    # adding cross references
+    motif_fams = fetch_value_list(motif_acc, rs.MOTIF_FAMS)
+    cross_ref_dict["RFAM"] = motif_fams
+    build_cross_references(entry, cross_ref_dict)
+
+    build_additional_fields(entry, motif_fields, 0, entry_type=entry_type)
+
 
 # ----------------------------------------------------------------------------
 
@@ -190,16 +287,22 @@ def build_additional_fields(entry, fields, num_3d_structures, entry_type):
     ET.SubElement(add_fields, "field", name="entry_type").text = entry_type
 
     # adding authors
-    author_list = get_value_list(fields["author"], rs_conf.AUTH_DEL)
+    author_list = get_value_list(fields["author"], rs.AUTH_DEL)
+
+    # to be deleted when author name is corrected on the db
+    if author_list.count("Argasinska") > 0:
+        author_list.remove("J")
+        author_list.remove("Argasinska")
+        author_list.append("Argasinska J")
 
     for author in author_list:
         ET.SubElement(add_fields, "field", name="author").text = author
 
-    if entry_type == "Family":
+    if entry_type == rs.FAMILY:
 
         # number of species
         ET.SubElement(add_fields, "field", name="num_species").text = str(fields[
-            "number_of_species"])
+            "num_species"])
         # number of 3D structures
         ET.SubElement(
             add_fields, "field", name="num_3d_structures").text = str(num_3d_structures)
@@ -211,7 +314,7 @@ def build_additional_fields(entry, fields, num_3d_structures, entry_type):
             "num_full"])
 
         # rna types
-        rna_types = get_value_list(fields["type"], rs_conf.RNA_TYPE_DEL)
+        rna_types = get_value_list(fields["rna_type"], rs.RNA_TYPE_DEL)
 
         for rna_type in rna_types:
             ET.SubElement(add_fields, "field", name="rna_type").text = rna_type
@@ -228,36 +331,15 @@ def build_additional_fields(entry, fields, num_3d_structures, entry_type):
     else:
         num_families = None
 
-        if entry_type == "Motif":
+        if entry_type == 'Motif':
             num_families = fetch_value(
-                rs_conf.NUM_FAMS_MOTIF, fields["motif_acc"])
+                rs.NUM_FAMS_MOTIF, fields["id"])
 
-        elif entry_type == "Clan":
-            num_families = fetch_value(
-                rs_conf.NUM_FAMS_CLAN, fields["clan_acc"])
+        elif entry_type == 'Clan':
+            num_families = fields["num_families"]
 
         ET.SubElement(add_fields, "field", name="num_families").text = str(
             num_families)
-
-# ----------------------------------------------------------------------------
-
-
-def motif_xml_builder():
-    '''
-        TO BE IMPLEMENTED
-    '''
-
-    pass
-
-# ----------------------------------------------------------------------------
-
-
-def clan_xml_builder():
-    '''
-        TO BE IMPLEMENTED
-    '''
-
-    pass
 
 # ----------------------------------------------------------------------------
 
@@ -269,6 +351,8 @@ def create_cross_ref_dict(db_name, db_keys, cross_refs):
 
     pass
 # ----------------------------------------------------------------------------
+
+# move this to db_utils
 
 
 def get_value_list(val_str, delimeter=','):  # done
@@ -289,10 +373,8 @@ def get_value_list(val_str, delimeter=','):  # done
 
 # ----------------------------------------------------------------------------
 
-# maybe move this to DB utils
-
-
-def fetch_value_list(rfam_acc, query):
+# maybe move this to db_utils
+def fetch_value_list(rfam_acc, query):  # done
     '''
         Retrieves and returns a list of all rfam_acc related values, returned
         by executing the query. Values in list are converted to string format.
@@ -313,6 +395,39 @@ def fetch_value_list(rfam_acc, query):
     cnx.disconnect()
 
     return map(lambda x: str(x[0]), pdb_structures)
+
+# ----------------------------------------------------------------------------
+
+
+def fetch_entry_fields(entry_acc, entry_type):  # done
+    '''
+        Returns a dictionary with the entry's fields.
+    '''
+
+    cnx = RfamDB.connect()
+    cursor = cnx.cursor(dictionary=True)
+
+    entry_type = entry_type[0].capitalize()
+
+    try:
+        if entry_type == rs.FAMILY:
+            cursor.execute(rs.FAM_QUERY % entry_acc)
+
+        elif entry_type == rs.CLAN:
+            cursor.execute(rs.CLAN_QUERY % entry_acc)
+
+        elif entry_type == rs.MOTIF:
+            cursor.execute(rs.MOTIF_QUERY % entry_acc)
+
+        fields = cursor.fetchall()[0]
+
+    except:
+        print "Failure retrieving values for entry %s." % entry_acc
+
+    cursor.close()
+    cnx.disconnect()
+
+    return fields
 
 # ----------------------------------------------------------------------------
 
@@ -347,6 +462,15 @@ def fetch_value(query, accession):
 
 # ----------------------------------------------------------------------------
 
+
+def usage():
+    '''
+        TO BE IMPLEMENTED
+    '''
+    pass
+
+# ----------------------------------------------------------------------------
+
 if __name__ == '__main__':
-    outdir = "/Users/ikalvari/Desktop/EB_eye_search"
-    xml4db_dumper(outdir)
+
+    pass

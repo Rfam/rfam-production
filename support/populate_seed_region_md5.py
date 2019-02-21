feb64bb9d2021a779aa703f85b99f373167118d1
ikalvari <ikalvari@ebi.ac.uk>
2019-02-21 14:15:01 +0000

    Minor improvements including sequence extraction log

--- support/populate_seed_region_md5.py
+++ support/populate_seed_region_md5.py
@@ -1,29 +1,25 @@
-import sys
 import hashlib
+import os
 import subprocess
+import sys
 
-from config import rfam_config
 from utils import seed_region as sr
-
-ESL_PATH = rfam_config.ESL_PATH
-
 
 # -------------------------------------------------------------------------
 
-def fetch_sequence(seq_file, seq_acc, seq_start, seq_end):
+def fetch_sequence_from_seed(rfam_seed_file, seq_acc, seq_start, seq_end):
     """
     Extracts a sequence from sequence file seq_file using rfamseq_acc
     and sequence start-end positions (seq_start, seq_end)
-    seq_file: A sequence file in fasta format to extract a sequence from
+    rfam_seed_file: A sequence file in fasta format to extract a sequence from
     seq_acc: The accession of the sequence to extract
     seq_start: The starting position of the sequence/subsequence
     seq_end: The end position of the sequence/subsequence
 
     return: A string which corresponds to the extracted sequence
     """
-    cmd = "%s %s %s/%s-%s" % (ESL_PATH,
-                                 seq_file, str(seq_acc),
-				 str(seq_start), str(seq_end))
+    cmd = "esl-sfetch %s %s/%s-%s" % (seq_file, str(seq_acc),
+                              str(seq_start), str(seq_end))
 
     proc = subprocess.Popen(
         cmd, shell=True, stdout=subprocess.PIPE)
@@ -37,6 +33,7 @@ def fetch_sequence(seq_file, seq_acc, seq_start, seq_end):
 
     return sequence
 
+
 # -------------------------------------------------------------------------
 
 if __name__ == '__main__':
@@ -47,34 +44,44 @@ if __name__ == '__main__':
 
     sql_data_list = []
 
+    fp_err = None
+
     for seq_region in seed_region_rows:
         seq_acc = seq_region[1]
         seq_start = seq_region[2]
         seq_end = seq_region[3]
+        rfam_acc = seq_region[0]
 
         # extract sequence from sequence file
-        sequence = fetch_sequence(seq_file, seq_acc, seq_start, seq_end)
+        sequence = fetch_sequence_from_seed(seq_file, seq_acc, seq_start, seq_end)
 
-        # Replace Us with Ts as done by RNAcentral
-        sequence = sequence.replace('U', 'T')
-        
-	if sequence != '' and len(sequence) > 2:
-		# convert sequence to md5
-        	m = hashlib.md5()
-        	m.update(sequence)
-        	seq_md5 = m.hexdigest()
-		# update tuple
-        	rfam_acc = seq_region[0]
-        	extended_tuple = (seq_md5, rfam_acc, seq_acc, seq_start, seq_end)
-        	# add to new list
-        	sql_data_list.append(extended_tuple)
-        	extended_tuple = None
+        # Replace Ts with Us as done by RNAcentral
+        # Note: there shouldn't be any in the seed alignments
+        sequence = sequence.replace('T', 'U')
 
-	else:
-		print seq_region
-    # update seed_region md5s
-    #sr.update_seed_region_md5s(sql_data_list)
+        if sequence != '' and len(sequence) > 2:
+            # convert sequence to md5
+            m = hashlib.md5()
+            m.update(sequence)
+            seq_md5 = m.hexdigest()
+            # update tuple
+            extended_tuple = (seq_md5, rfam_acc, seq_acc, seq_start, seq_end)
+            # add to new list
+            sql_data_list.append(extended_tuple)
+            extended_tuple = None
 
+        else:
+            # check if first error
+            if fp_err is None:
+                fp_err = open(os.path.join(os.path.split(seq_file)[0], "seed_md5.err"), 'w')
 
+            fp_err.write("Error extracting sequence %s at %s-%s of %s\n" % (seq_acc,
+                                                                            seq_start,
+                                                                            seq_end,
+                                                                            rfam_acc))
 
-
+    # update/populate table
+    sr.update_seed_region_md5s(sql_data_list)
+    # close error file if opened
+    if fp_err is not None:
+        fp_err.close()

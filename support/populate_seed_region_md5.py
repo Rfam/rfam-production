59fd25d8c4ba182093a80b66c32b8e8263bfda27
ikalvari <ikalvari@ebi.ac.uk>
2019-02-21 18:01:19 +0000

    Tidy up code and add regions options seed/full

--- support/populate_seed_region_md5.py
+++ support/populate_seed_region_md5.py
@@ -4,10 +4,11 @@ import subprocess
 import sys
 
 from utils import seed_region as sr
+from utils import db_utils as db
 
 # -------------------------------------------------------------------------
 
-def fetch_sequence_from_seed(rfam_seed_file, seq_acc, seq_start, seq_end):
+def fetch_sequence(seq_file, seq_acc, seq_start, seq_end, type='seed'):
     """
     Extracts a sequence from sequence file seq_file using rfamseq_acc
     and sequence start-end positions (seq_start, seq_end)
@@ -18,9 +19,14 @@ def fetch_sequence_from_seed(rfam_seed_file, seq_acc, seq_start, seq_end):
 
     return: A string which corresponds to the extracted sequence
     """
-    cmd = "esl-sfetch %s %s/%s-%s" % (seq_file, str(seq_acc),
+    cmd = ''
+    if type == 'seed':
+        cmd = "esl-sfetch %s %s/%s-%s" % (seq_file, str(seq_acc),
                               str(seq_start), str(seq_end))
 
+    elif type == 'full':
+        cmd = "esl-sfetch -c %s..%s %s -%s" % (str(seq_start), str(seq_end),
+                                               seq_file, str(seq_acc))
     proc = subprocess.Popen(
         cmd, shell=True, stdout=subprocess.PIPE)
 
@@ -33,27 +39,42 @@ def fetch_sequence_from_seed(rfam_seed_file, seq_acc, seq_start, seq_end):
 
     return sequence
 
-
 # -------------------------------------------------------------------------
 
-if __name__ == '__main__':
+def generate_md5s_and_populate_table(seq_file, type = "seed", dest_dir = None):
+    """
+    Fetches seed of full regions from the database and populates/updates the
+    appropriate table with the md5s of the corresponding ncRNA sequences.
 
-    seq_file = sys.argv[1]
+    seq_file: This can be the Rfam.seed file (seed) or an Rfamseq file (full)
+    type: one of seed/full
 
-    seed_region_rows = sr.fetch_seed_regions()
+    return: void
+    """
+
+    if dest_dir is None:
+        dest_dir = os.path.split(seq_file)[0]
+
+
+    region_rows = None
+
+    if type == "seed":
+        region_rows = sr.fetch_seed_regions()
+    elif type == "full":
+        region_rows = db.fetch_metagenomic_regions()
 
     sql_data_list = []
 
     fp_err = None
 
-    for seq_region in seed_region_rows:
+    for seq_region in region_rows:
         seq_acc = seq_region[1]
         seq_start = seq_region[2]
         seq_end = seq_region[3]
         rfam_acc = seq_region[0]
 
         # extract sequence from sequence file
-        sequence = fetch_sequence_from_seed(seq_file, seq_acc, seq_start, seq_end)
+        sequence = fetch_sequence(seq_file, seq_acc, seq_start, seq_end, type)
 
         # Replace Ts with Us as done by RNAcentral
         # Note: there shouldn't be any in the seed alignments
@@ -73,7 +94,7 @@ if __name__ == '__main__':
         else:
             # check if first error
             if fp_err is None:
-                fp_err = open(os.path.join(os.path.split(seq_file)[0], "seed_md5.err"), 'w')
+                fp_err = open(os.path.join(dest_dir, "md5.err"), 'w')
 
             fp_err.write("Error extracting sequence %s at %s-%s of %s\n" % (seq_acc,
                                                                             seq_start,
@@ -81,7 +102,30 @@ if __name__ == '__main__':
                                                                             rfam_acc))
 
     # update/populate table
-    sr.update_seed_region_md5s(sql_data_list)
+    if type == "seed":
+        sr.update_seed_region_md5s(sql_data_list)
+    else:
+        db.update_metagenomic_region_md5s(sql_data_list)
+
     # close error file if opened
     if fp_err is not None:
         fp_err.close()
+
+
+# -------------------------------------------------------------------------
+if __name__ == '__main__':
+
+    seq_file = sys.argv[1]
+    type_flag = sys.argv[2]
+
+    if type_flag.lower() == "--seed":
+        type = "seed"
+    elif type_flag.lower() == "--full":
+        type = "full"
+    else:
+        print "\nWrong selection of type: %s\n" % type_flag
+
+    generate_md5s_and_populate_table(seq_file, type="seed", dest_dir=None)
+
+
+

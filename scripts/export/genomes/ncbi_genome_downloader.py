8229617c7b0c3da4b46093cee8f81a55a8eff81a
ikalvari <ikalvari@ebi.ac.uk>
2020-11-18 16:07:28 +0200

    Implements function to load accession list from file

--- scripts/export/genomes/ncbi_genome_downloader.py
+++ scripts/export/genomes/ncbi_genome_downloader.py
@@ -19,6 +19,27 @@ def gcf_report_parser(gcf_report):
 
     return accessions
 
+
+# -----------------------------------------------------------------------------
+
+
+
+def load_accession_list(accession_list):
+    """
+    Parses a GCF report file from NCBI and returns all the NCBI accessions
+
+    gcf_report:
+
+    return: a list of wgs accessions listed in the gcf report file
+    """
+
+    fp = open(accession_list, 'r')
+
+    accessions = [x.strip() for x in fp]
+
+    return accessions
+
+
 # -----------------------------------------------------------------------------
 
 
@@ -27,7 +48,9 @@ if __name__ == '__main__':
     gcf_report_file = sys.argv[1]
     dest_dir = sys.argv[2]
 
-    accessions = gcf_report_parser(gcf_report_file)
+    #accessions = gcf_report_parser(gcf_report_file)
+
+    accessions = load_accession_list(gcf_report_file)
 
     for accession in accessions:
         gf.download_fasta_from_ncbi(accession, dest_dir=dest_dir)

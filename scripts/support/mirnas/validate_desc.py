eec9bb524e43fc2c66818f7ea35c7e4a94897941
ikalvari <ikalvari@ebi.ac.uk>
2020-11-24 18:35:32 +0000

    Fixes variable name

--- scripts/support/mirnas/validate_desc.py
+++ scripts/support/mirnas/validate_desc.py
@@ -8,7 +8,7 @@ from subprocess import Popen, PIPE
 # ------------------------------------------------------------------------------------------
 
 
-searchdirs = ["/hps/nobackup/production/xfam/rfam/RELEASES/14.3/miRNA_relabelled/batch1_chunk1_searches",
+search_dirs = ["/hps/nobackup/production/xfam/rfam/RELEASES/14.3/miRNA_relabelled/batch1_chunk1_searches",
               "/hps/nobackup/production/xfam/rfam/RELEASES/14.3/miRNA_relabelled/batch1_chunk2_searches",
               "/hps/nobackup/production/xfam/rfam/RELEASES/14.3/miRNA_relabelled/batch2/searches"]
 

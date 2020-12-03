46b3ef29dca3456d225e3d6de0764a0c0ec0e1ad
ikalvari <ikalvari@ebi.ac.uk>
2020-12-03 17:09:19 +0000

    Implements option for serial rfmake execution

--- scripts/support/mirnas/automake.py
+++ scripts/support/mirnas/automake.py
@@ -3,6 +3,10 @@ import json
 import subprocess
 import argparse
 
+
+# -------------------------------------------------------------------------------------------------------
+
+
 searchdirs = ["/hps/nobackup/production/xfam/rfam/RELEASES/14.3/miRNA_relabelled/batch1_chunk1_searches", 
               "/hps/nobackup/production/xfam/rfam/RELEASES/14.3/miRNA_relabelled/batch1_chunk2_searches",
               "/hps/nobackup/production/xfam/rfam/RELEASES/14.3/miRNA_relabelled/batch2/searches"]
@@ -11,12 +15,15 @@ MEMORY = 15000
 CPU = 4
 LSF_GROUP = "/family_srch"
 
+# -------------------------------------------------------------------------------------------------------
 
-def autorfmake(thresholds_file):
+
+def autorfmake(thresholds_file, serial=False):
     
     cmd = ("bsub -M %s -o %s -e %s -n %s -g %s -q production-rh74 "
                "-J %s \"cd %s && rfmake.pl -t %s -forcethr -a\"")
 
+    serial_cmd = "rfmake.pl -t %s -forcethr"
 
     fp = open(thresholds_file, 'r')
     thresholds = json.load(fp)
@@ -31,15 +38,22 @@ def autorfmake(thresholds_file):
                 family_dir = os.path.join(searchdir, family)
 
             if os.path.exists(family_dir):
-                lsf_err_file = os.path.join(family_dir, "auto_rfmake.err")
-                lsf_out_file = os.path.join(family_dir, "auto_rfmake.out")
-		job_name = family
 
-  		subprocess.call(cmd % (MEMORY, lsf_out_file, lsf_err_file, CPU, LSF_GROUP, job_name, family_dir, str(thresholds[family])), shell=True)
+		if serial is True:
+			os.chdir(family_dir)
+			subprocess.call(serial_cmd % str(thresholds[family])), shell=True)
+
+		else:
+			lsf_err_file = os.path.join(family_dir, "auto_rfmake.err")
+                	lsf_out_file = os.path.join(family_dir, "auto_rfmake.out")
+                	job_name = family
+  		
+			subprocess.call(cmd % (MEMORY, lsf_out_file, lsf_err_file, CPU, LSF_GROUP, job_name, family_dir, str(thresholds[family])), shell=True)
             else:
                 continue
     fp.close()
 
+# -------------------------------------------------------------------------------------------------------
 
 
 def parse_arguments():
@@ -47,10 +61,11 @@ def parse_arguments():
 	parser = argparse.ArgumentParser()
 	
 	parser.add_argument("--thresholds", help="A json file with miRNA : threshold pairs", action="store")
+	parser.add_argument("--serial", help="Serial execution of rfmake", action="store_true", default=False)
 	
 	return parser
 
-
+# -------------------------------------------------------------------------------------------------------
 
 if __name__=='__main__':
 

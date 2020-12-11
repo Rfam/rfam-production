1d47b8230892da0a1e78128971a19ca8b900c3cc
ikalvari <ikalvari@ebi.ac.uk>
2020-12-11 21:37:45 +0000

    Basic argument parsing

--- scripts/export/parallel_xml_dumper.py
+++ scripts/export/parallel_xml_dumper.py
@@ -4,18 +4,41 @@ in order to speed up Rfam exports. It uses rfam_xml_dumper and submits an indivi
 lsf job for each entry provided in the input file  
 """
 
+# ----------------------------------------------------------------------------------------
 
 import os
-import sys
 import subprocess
+import argparse
+
+# ----------------------------------------------------------------------------------------
+
+def parse_arguments():
+
+	parser = argparse.ArgumentParser()
+	
+	parser.add_argument("--accessions", help="Rfam list of accessions to index", 
+			action="store", required=True)
+	parser.add_argument("--acc-type", help="rfam entry type (F: Family, M: Motif, C: Clan, G: Genome, R: Regions)",
+                          type=str, choices=['F', 'M', 'C', 'G', 'R'], required=True)
+
+	parser.add_argument("--dest-dir", help="Destination directory for the output", 
+			action="store", required=True)
+
+	return parser
+
+# ----------------------------------------------------------------------------------------
+
 
 if __name__ == '__main__':
 
+	parser = parse_arguments()
+	args = parser.parse_args()
+
 	path_to_xml_dump = os.path.join(os.getcwd(), "rfam_xml_dumper.py")
 
-	rfam_accession_file = sys.argv[1]
-	acc_type = sys.argv[2]
-	dest_dir = sys.argv[3]	
+	rfam_accession_file = args.accessions
+	acc_type = args.acc_type
+	dest_dir = args.dest_dir
 
 	fp = open(rfam_accession_file, 'r')
 	

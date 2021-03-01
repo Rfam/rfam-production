926b1a36d896e69c9bd4aa994f66ae3ce152927e
ikalvari <ikalvari@ebi.ac.uk>
2021-03-01 15:49:58 +0000

    Adds option to parse clan accessions from file

--- scripts/release/clan_file_generator.py
+++ scripts/release/clan_file_generator.py
@@ -24,6 +24,26 @@ from utils import db_utils as rfamdb
 # -----------------------------------------------------------------------------
 
 
+def parse_clan_file(clan_list):
+    """
+    Parses a list of Rfam clan accessions
+
+    clan_list: A plain .txt file containing a list of Rfam Clan Accessions
+
+    return: A list of clan accessions
+    """
+
+    fp = open(clan_list, 'r')
+
+    clan_accessions = [x.strip() for x in fp]
+
+    fp.close()
+
+    return clan_accessions
+
+# -----------------------------------------------------------------------------
+
+
 def clan_file_generator(output_dir, clan_comp_type='FULL', clan_acc=None):
     """
     Generates clan files for clan competition
@@ -44,13 +64,19 @@ def clan_file_generator(output_dir, clan_comp_type='FULL', clan_acc=None):
     if clan_acc is None:
         clans = rfamdb.fetch_clan_accessions()
 
-    else:
+    elif os.path.isfile(clan_acc):
+        # fetches clan accessions from file
+        clans = parse_clan_file(clan_acc)
+
+    elif clan_acc[0:2] == "CL":
         clans.append(clan_acc)
+
+    else:
+        sys.exit("Wrong clan input provided!")
 
     # some sanity checks
     if clan_comp_type.upper() != 'FULL' and clan_comp_type.upper() != 'PDB':
         sys.exit("\nPlease provide correct type for clan competition!")
-
 
     # generate clan files
     for clan in clans:
@@ -105,9 +131,17 @@ def parse_arguments():
 
     parser = argparse.ArgumentParser(description='Generates required clan competition input files')
 
-    parser.add_argument("--dest-dir", help="Destination directory where to generate the files", type=str)
-    parser.add_argument("--cc-type", help="Specifies clan competition type FULL/PDB", type=str)
-    parser.add_argument("--clan-acc", help="Clan accession", type=str)
+    mutualy_exclusive = parser.add_mutually_exclusive_group()
+    parser.add_argument("--dest-dir", help="Destination directory where to generate the files",
+                        type=str, action="store")
+    parser.add_argument("--cc-type", help="Specifies clan competition type FULL/PDB", type=str,
+                        action="store")
+    mutualy_exclusive.add_argument("--clan-acc", help="Clan accession", type=str)
+    mutualy_exclusive.add_argument("--all", help="All clans in Rfam",
+                                   action="store_true", default=False)
+    mutualy_exclusive.add_argument("-f",
+                                   help="A file with a list of clan accessions to compete",
+                                   action="store")
 
     return parser
 
@@ -125,7 +159,12 @@ if __name__ == '__main__':
     if args.clan_acc:
         clan_file_generator(args.dest_dir, args.cc_type, args.clan_acc)
 
+    elif args.all:
+            clan_file_generator(args.dest_dir, args.cc_type, None)
+
     else:
-        clan_file_generator(args.dest_dir, args.cc_type, None)
+        clan_file_generator(args.dest_dir, args.cc_type, args.f)
+
+
 
 

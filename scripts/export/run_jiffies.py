078d3bc63a293335b757236634c4f7065de39673
ikalvari <ikalvari@ebi.ac.uk>
2021-03-04 16:10:50 +0000

    Makes caller function atomic and uses more descriptive variable names

--- scripts/export/run_jiffies.py
+++ scripts/export/run_jiffies.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 
 """
-Copyright [2009-2017] EMBL-European Bioinformatics Institute
+Copyright [2009-2021] EMBL-European Bioinformatics Institute
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
@@ -13,87 +13,98 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 
-"""
-Description: Support code designed to ease Rfam jiffies' execution.
-
-Notes: 1. Call this script as rfamprod
-"""
-
 # ---------------------------------IMPORTS-------------------------------------
 
-import os
-import sys
 import argparse
+import os
 import subprocess
+import sys
 
 JIFFIES = {"SEED": "writeAnnotatedSEED.pl",
-	"CM": "writeAnnotatedCM.pl",
-	"TREE": "writeAnnotatedTree.pl"}
+           "CM": "writeAnnotatedCM.pl",
+           "TREE": "writeAnnotatedTree.pl"}
 
 # -----------------------------------------------------------------------------
 
 
-def call_jiffy(jiffy_type, fam_file, outdir=None):
+def export_ftp_file(type, rfam_acc, dest_dir=None):
     """
-    This function was designed to call the perl script defined by the jiffy
-    parameter. Used in Rfam 12.1 with jiffies writeAnnotatedCM.pl,
-    writeAnnotatedSeed.pl and writeAnnotatedTree.pl to generate CM, SEED
-    and seed_Tree files for the FTP server
 
-    jiffy:  The path to the perl script to call
-    fam_file:   A list of all rfam_accessions
-    outdir: Destination directory where the files will be generated
+    :param type:
+    :param rfam_acc:
+    :param dest_dir:
+    :return:
     """
 
     # TO DO
     # convert this to load family accessions from the database
 
-    if outdir is not None:
-        os.chdir(os.path.abspath(outdir))
+    if dest_dir is not None:
+        os.chdir(os.path.abspath(dest_dir))
+    else:
+        sys.exit("Provide a valid destination directory")
 
-    fp = open(os.path.abspath(fam_file), 'r')
+    try:
+        # select jiffy
+        jiffy = JIFFIES[type]
 
-    for rfam_acc in fp:
+        # call jiffy
         cmd = "perl %s %s" % (jiffy, rfam_acc)
         subprocess.call(cmd, shell=True)
 
-    fp.close()
-
+    except:
+        raise OSError
 
 # -----------------------------------------------------------------------------
 
 
 def parse_arguments():
+    """
+    """
 
-	"""
-	"""
+    parser = argparse.ArgumentParser()
 
-	parser = argparse.ArgumentParser()
-	
-	mutually_exclusive_type = parser.add_mutually_exclusive_group()
-	mutually_exclusive_type.add_argument("--seed", help="Export SEED files from SVN", 
-		action="store_true", default=False)
-	mutually_exclusive_type.add_argument("--cm", help="Export CM files from SVN", 
-                action="store_true", default=False)
-	mutually_exclusive_type.add_argument("--tree", help="Export SEED files from SVN", 
-                action="store_true", default=False)
+    mutually_exclusive_type = parser.add_mutually_exclusive_group()
+    mutually_exclusive_type.add_argument("--seed", help="Export SEED files from SVN",
+                                         action="store_true", default=False)
+    mutually_exclusive_type.add_argument("--cm", help="Export CM files from SVN",
+                                         action="store_true", default=False)
+    mutually_exclusive_type.add_argument("--tree", help="Export SEED files from SVN",
+                                         action="store_true", default=False)
 
-	mutually_exclusive_input = parser.add_mutually_exclusive_group()
-	mutually_exclusive_input.add_argument("--acc", help="Rfam family accession",
-                action="store",type=str)
-	mutually_exclusive_input.add_argument("-f", help="List of Rfam family accessions (.txt)",
-                action="store",type=str)
+    mutually_exclusive_input = parser.add_mutually_exclusive_group()
+    mutually_exclusive_input.add_argument("--acc", help="Rfam family accession",
+                                          action="store", type=str)
+    mutually_exclusive_input.add_argument("-f", help="List of Rfam family accessions (.txt)",
+                                          action="store", type=str)
 
-	parser.add_argument("--dest-dir", help="Destination directory to generate files to")
+    parser.add_argument("--dest-dir", help="Destination directory to generate files to")
 
-	return parser
+    return parser
 
 # -----------------------------------------------------------------------------
 
 if __name__ == '__main__':
-    # need to check the number of parameters provided
-    jiffy = sys.argv[1]
-    fam_file = sys.argv[2]
-    outdir = sys.argv[3]
 
-    call_jiffy(jiffy, fam_file, outdir)
+    parser = parse_arguments()
+    args = parser.parse_args()
+
+    accessions = []
+    if os.path.isfile(args.f):
+        fp = open(args.f, 'r')
+        accessions = [acc.strip() for acc in fp]
+        fp.close()
+    elif args.acc[0:2] == 'RF':
+        accessions.append(args.acc)
+
+    jiffy_type = None
+
+    if args.seed:
+        jiffy_type = "SEED"
+    elif args.cm:
+        jiffy_type = "CM"
+    elif args.tree:
+        jiffy_type = "TREE"
+
+    for accession in accessions:
+        export_ftp_file(jiffy_type, accession, args.dest_dir)

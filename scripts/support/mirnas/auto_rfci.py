00e19db5f1a0b1ae1b177801d6f5b5988ac69421
Emma Cooke <emma.cooke93@gmail.com>
2021-10-13 18:16:01 +0100

    run rfci.pl with yes prompt

--- scripts/support/mirnas/auto_rfci.py
+++ scripts/support/mirnas/auto_rfci.py
@@ -3,15 +3,16 @@ import argparse
 import subprocess
 import time
 
-from scripts.support.mirnas.update_mirnas_helpers import UPDATE_DIR, MEMORY, CPU, LSF_GROUP
+from scripts.support.mirnas.update_mirnas_helpers import UPDATE_DIR
 
 
-def check_error_file(family):
+def check_svn_error(family):
     family_dir = os.path.join(UPDATE_DIR, family)
-    err_file = os.path.join(family_dir, "auto_rfci.err")
-    with open(err_file) as f:
-        f_read = f.read()
-        if "Filesystem has no item: '/trunk/Families/{0}/SEEDTBLOUT'".format(family) in f_read:
+    out_file = os.path.join(family_dir, "auto_rfci.out")
+
+    with open(out_file) as rqc_output:
+        read_output = rqc_output.read()
+        if "Filesystem has no item: '/trunk/Families/{0}/SEEDTBLOUT'".format(family) in read_output:
             print('svn add for this family')
             os.chdir(family_dir)
             subprocess.call('svn add SEEDSCORES SEEDTBLOUT', shell=True)
@@ -22,9 +23,9 @@ def check_error_file(family):
 
 def check_successful(rfam_acc):
     family_dir = os.path.join(UPDATE_DIR, rfam_acc)
-    lsf_err_file = os.path.join(family_dir, "auto_rfci.err")
+    out_file = os.path.join(family_dir, "auto_rfci.out")
 
-    with open(lsf_err_file) as rqc_output:
+    with open(out_file) as rqc_output:
         read_output = rqc_output.read()
         if "Successfully checked family in" in read_output:
             return True
@@ -34,19 +35,14 @@ def check_successful(rfam_acc):
 
 def check_in(rfam_acc, preseed=False):
     family_dir = os.path.join(UPDATE_DIR, rfam_acc)
-    lsf_err_file = os.path.join(family_dir, "auto_rfci.err")
-    lsf_out_file = os.path.join(family_dir, "auto_rfci.out")
-    job_name = rfam_acc
+    out_file = os.path.join(family_dir, "auto_rfci.out")
     if preseed:
         option = "\'Update using miRBase seed\' -preseed"
     else:
         option = "\'Update using miRBase seed\'"
-    cmd = ("bsub -M {mem} -o {out_file} -e {err_file} -n {cpu} -g {lsf_group} -J {job_name} "
-           "\"cd {update_dir} && rfci.pl -m {option} {rfam_acc}\"")
-    subprocess.call(
-        cmd.format(
-            mem=MEMORY, out_file=lsf_out_file, err_file=lsf_err_file, cpu=CPU, lsf_group=LSF_GROUP,
-            job_name=job_name, update_dir=UPDATE_DIR, option=option, rfam_acc=rfam_acc), shell=True)
+    os.chdir(UPDATE_DIR)
+    subprocess.call("rfci.pl -m {option} {rfam_acc} > out_file".format(option=option, rfam_acc=rfam_acc,
+                                                                       out_file=out_file), shell=True)
 
 
 def check_in_all_families(families):
@@ -57,13 +53,14 @@ def check_in_all_families(families):
         all_families = [line for line in file_data.split('\n') if line.strip() != '']
         for family in all_families:
             check_in(family)
-            time.sleep(120)
+            time.sleep(60)
             if check_successful(family):
                 print('{0} has been checked in'.format(family))
                 checked_in.append(family)
             else:
-                svn_add = check_error_file(family)
+                svn_add = check_svn_error(family)
                 if svn_add:
+                    print('svn add has been applied for {0}. Trying to check in again using -preseed'.format(family))
                     check_in(family, preseed=True)
                     if check_successful(family):
                         print('{0} has been checked in'.format(family))

b5d7aa38fb2d2b9c8d43c0e1fbc1c1860b853927
Emma Cooke <emma.cooke93@gmail.com>
2021-10-22 09:59:04 +0100

    Update auto_rfci.py

--- scripts/support/mirnas/auto_rfci.py
+++ scripts/support/mirnas/auto_rfci.py
@@ -4,6 +4,7 @@ import subprocess
 import time
 
 from scripts.support.mirnas.config import UPDATE_DIR
+from scripts.support.mirnas.update_mirnas_helpers import MEMORY, CPU, LSF_GROUP
 
 checked_in = []
 not_checked_in = []
@@ -17,7 +18,7 @@ def check_svn_error(family):
     """
 
     family_dir = os.path.join(UPDATE_DIR, family)
-    out_file = os.path.join(family_dir, "auto_rfci.out")
+    out_file = os.path.join(family_dir, "auto_rfci.err")
 
     with open(out_file) as rqc_output:
         read_output = rqc_output.read()
@@ -38,7 +39,7 @@ def check_successful(rfam_acc):
     """
 
     family_dir = os.path.join(UPDATE_DIR, rfam_acc)
-    out_file = os.path.join(family_dir, "auto_rfci.out")
+    out_file = os.path.join(family_dir, "auto_rfci.err")
 
     with open(out_file) as rqc_output:
         read_output = rqc_output.read()
@@ -55,20 +56,20 @@ def check_in(acc, pre_seed=False, ignore_seed=False):
     :param pre_seed: True if using -preseed option, else False
     :param ignore_seed: True if using -i seed option, else False
     """
-
     family_dir = os.path.join(UPDATE_DIR, acc)
-    out_file = os.path.join(family_dir, "auto_rfci.out")
+    lsf_err_file = os.path.join(family_dir, "auto_rqc.err")
+    lsf_out_file = os.path.join(family_dir, "auto_rqc.out")
+    cmd = ("bsub -M {mem} -o {out_file} -e {err_file} -n {cpu} -g {lsf_group} -J {job_name} "
+           "\"cd {update_dir} && rfci.pl -m {msg} {option} {rfam_acc}\"")
     message = "\'Update using miRBase seed\'"
     option = ''
     if pre_seed:
         option += "-preseed "
     if ignore_seed:
         option += "-i seed "
-    os.chdir(UPDATE_DIR)
-    cmd = "rfci.pl -m {msg} {option} {rfam_acc} > {out_file} ".format(msg=message, option=option, rfam_acc=acc,
-                                                                      out_file=out_file)
-    process = subprocess.Popen([cmd], shell=True)
-    process.communicate()
+    cmd.format(mem=MEMORY, out_file=lsf_out_file, err_file=lsf_err_file, cpu=CPU, lsf_group=LSF_GROUP, job_name=acc,
+               update_dir=UPDATE_DIR, msg=message, option=option, rfam_acc=acc)
+    subprocess.call(cmd, shell=True)
 
 
 def call_check_in(family, ignore_seed):
@@ -80,7 +81,7 @@ def call_check_in(family, ignore_seed):
     """
 
     check_in(family, ignore_seed=ignore_seed)
-    time.sleep(30)
+    time.sleep(20)
     if check_successful(family):
         print('{0} has been checked in'.format(family))
         checked_in.append(family)
@@ -95,7 +96,7 @@ def call_check_in(family, ignore_seed):
         else:
             not_checked_in.append(family)
             print('{0} HAS NOT been checked in. Please check manually {1}'.format(
-                family, os.path.join(UPDATE_DIR, family, "auto_rfci.out")))
+                family, os.path.join(UPDATE_DIR, family, "auto_rfci.err")))
 
 
 def write_result_to_files():

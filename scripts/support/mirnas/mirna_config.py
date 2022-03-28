ee0d3ecf32cfc652033aab6d2558b318b6a63cf5
Emma Cooke <emma.cooke93@gmail.com>
2022-03-28 14:48:43 +0100

    add batch3 path to config

--- scripts/support/mirnas/mirna_config.py
+++ scripts/support/mirnas/mirna_config.py
@@ -1,8 +1,9 @@
-UPDATE_DIR = "/hps/nobackup/production/xfam/rfam/RELEASES/14.3/miRNA_relabelled/update_old_rfam_mirnas"
+UPDATE_DIR = "/hps/nobackup/production/xfam/rfam/RELEASES/14.8/microrna/update_old_rfam_mirnas"
 MIRNAS_CSV = "rfam-production/mirnas_sample.csv"
 SEARCH_DIRS = ["/hps/nobackup/production/xfam/rfam/RELEASES/14.3/miRNA_relabelled/batch1_chunk1_searches",
                "/hps/nobackup/production/xfam/rfam/RELEASES/14.3/miRNA_relabelled/batch1_chunk2_searches",
-               "/hps/nobackup/production/xfam/rfam/RELEASES/14.3/miRNA_relabelled/batch2/searches"]
+               "/hps/nobackup/production/xfam/rfam/RELEASES/14.3/miRNA_relabelled/batch2/searches",
+               "/hps/nobackup/production/xfam/rfam/RELEASES/14.8/microrna/batch3"]
 NEW_DIR = "/hps/nobackup/production/xfam/rfam/RELEASES/14.8/microrna/new_families"
 ENV_PATH = "/nfs/production/xfam/users/rfamprod/code/env2/bin/activate"
 DESC_GEN_PATH = "/nfs/production/xfam/users/rfamprod/code/rfam-production/scripts/preprocessing/desc_generator.py"

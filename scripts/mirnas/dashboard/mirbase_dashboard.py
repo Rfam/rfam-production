7236423f007d6919ed0f200cb2afbcb427409cb9
Emma Cooke <emma.cooke93@gmail.com>
2022-06-21 15:52:18 +0100

    update imports

--- scripts/mirnas/dashboard/mirbase_dashboard.py
+++ scripts/mirnas/dashboard/mirbase_dashboard.py
@@ -10,23 +10,14 @@ import argparse
 import csv
 import os
 
-from compute_dashboard_entries import run_rfmake, get_overlaps, is_inconsistent_ss, \
-    get_id_matches, get_action
+from compute_dashboard_entries import run_rfmake, get_overlaps, is_inconsistent_ss, get_id_matches, get_action
 from dashboard_exceptions import SpeciesFileNotFound
-from format_dashboard import get_header_line, get_input_data, format_rfam_overlaps, \
-    format_mirbase_url, format_rfam_ids, get_summary, get_google_sheets_data
+from format_dashboard import (get_header_line, get_input_data, format_rfam_overlaps, format_mirbase_url,
+                              format_rfam_ids, get_summary, get_google_sheets_data)
 from microrna_progress import updated_families, new_commits
-from getters import get_output_path, get_output_url, get_family_location, get_report_url, \
-    get_mirbase_alignments, get_mirbase_id, get_mirbase_acc
-
-HTML_REPORTS = '/nfs/public/rw/xfam/rfam/' + 'searches/mirbase'
-OUTPUT_FILENAME = 'mirbase-dashboard.tsv'
-BLACK_LIST = [
-    'MIPF0000338__mir-680'
-    'MIPF0000419__mir-574',
-    'MIPF0000901__mir-3470',
-    'MIPF0001768__mir-7407',  # rfmake running
-]  # giant families that cause crashes
+from getters import (get_output_path, get_output_url, get_family_location, get_report_url, get_mirbase_alignments,
+                     get_mirbase_id, get_mirbase_acc)
+from dashboard_config import OUTPUT_FILENAME, BLACK_LIST
 
 
 def qc_check(new, updated):

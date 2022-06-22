bfff970bf4379e90ac1c8a16d4d18b77c28fed56
Emma Cooke <emma.cooke93@gmail.com>
2022-06-22 11:00:52 +0100

    extract constants to config

--- scripts/mirnas/dashboard/compare_rfam_mirbase.py
+++ scripts/mirnas/dashboard/compare_rfam_mirbase.py
@@ -20,12 +20,18 @@ import requests
 from dashboard_config import HTML_REPORTS, COMPARE_OUTPUT_FILENAME
 from getters import get_output_url
 
+MIRBASE_ENTRY = 'https://www.mirbase.org/cgi-bin/mirna_entry.pl?acc'
+RNACENTRAL_DESCRIPTION = 'https://www.ebi.ac.uk/ebisearch/ws/rest/rnacentral/entry/{}?format=json&fields=description'
+RNACENTRAL_MIRBASE_TSV = ('https://ftp.ebi.ac.uk/pub/databases/RNAcentral/current_release/id_mapping/'
+                          'database_mappings/mirbase.tsv')
+RFAM_SVN = 'https://xfamsvn.ebi.ac.uk/svn'
+
 
 def get_last_modified_date(rfam_acc):
     """
     Get the last modified date of a SEED file in Rfam SVN.
     """
-    cmd = 'svn log --xml  https://xfamsvn.ebi.ac.uk/svn/data_repos/trunk/Families/{}/SEED'.format(rfam_acc)
+    cmd = 'svn log --xml  {}/data_repos/trunk/Families/{}/SEED'.format(RFAM_SVN, rfam_acc)
     svn_output = subprocess.check_output(cmd, shell=True)
     root = ET.fromstring(svn_output)
     return root[0][1].text[:10]
@@ -89,12 +95,12 @@ def get_rnacentral_mirbase_xrefs():
     filename = 'mirbase.tsv'
     data = {}
     if not os.path.exists(filename):
-        cmd = 'wget https://ftp.ebi.ac.uk/pub/databases/RNAcentral/current_release/id_mapping/database_mappings/mirbase.tsv'
+        cmd = 'wget %s' % RNACENTRAL_MIRBASE_TSV
         os.system(cmd)
     with open(filename, 'r') as f:
         for line in f:
             parts = line.split('\t')
-            data[parts[0]+'_'+parts[3]] = parts[2]
+            data[parts[0] + '_' + parts[3]] = parts[2]
     return data
 
 
@@ -103,25 +109,25 @@ def get_rnacentral_description(urs_taxid):
     Fetch sequence description using RNAcentral text search API.
     """
     description = ''
-    url = 'https://www.ebi.ac.uk/ebisearch/ws/rest/rnacentral/entry/{}?format=json&fields=description'
+    url = RNACENTRAL_DESCRIPTION
     data = requests.get(url.format(urs_taxid))
     if data.status_code == 200:
         description = data.json()['entries'][0]['fields']['description'][0]
     return description
 
 
-
 def display_non_mirna_matching_accs(non_mirna_matching_accs, cmscan_data):
     mirbase_xrefs = get_rnacentral_mirbase_xrefs()
     filename = os.path.join(HTML_REPORTS, COMPARE_OUTPUT_FILENAME)
-    header = ['Family', 'Rfam ID', 'RNA type', 'Rfam description', 'RNAcentral ID', 'miRBase ID', 'RNAcentral description', 'Last updated: ' + '{}'.format(time.strftime("%d/%m/%Y %H:%M"))]
+    header = ['Family', 'Rfam ID', 'RNA type', 'Rfam description', 'RNAcentral ID', 'miRBase ID',
+              'RNAcentral description', 'Last updated: ' + '{}'.format(time.strftime("%d/%m/%Y %H:%M"))]
     f_out = open(filename, 'w')
     f_out.write('\t'.join(header) + '\n')
     for rfam_acc in non_mirna_matching_accs:
         for seq in cmscan_data[rfam_acc]:
             metadata = db.fetch_family_metadata(rfam_acc)
             if seq in mirbase_xrefs:
-                mirbase_url = '=HYPERLINK("https://www.mirbase.org/cgi-bin/mirna_entry.pl?acc={0}", "{0}")'.format(mirbase_xrefs[seq])
+                mirbase_url = '=HYPERLINK("{1}={0}", "{0}")'.format(mirbase_xrefs[seq], MIRBASE_ENTRY)
             else:
                 mirbase_url = ''
             line = '\t'.join([
@@ -166,7 +172,8 @@ def main():
     rfam_microrna_accs = db.fetch_mirna_families()
     rfam_accs = db.fetch_rfam_accs_sorted()
     get_rfam_microrna_families_without_mirbase_hits(rfam_microrna_accs, rfam_cmscan_accs)
-    non_mirna_matching_accs = get_rfam_non_microrna_families_matching_mirbase(rfam_accs, rfam_microrna_accs, rfam_cmscan_accs)
+    non_mirna_matching_accs = get_rfam_non_microrna_families_matching_mirbase(rfam_accs, rfam_microrna_accs,
+                                                                              rfam_cmscan_accs)
     display_non_mirna_matching_accs(non_mirna_matching_accs, cmscan_data)
 
 

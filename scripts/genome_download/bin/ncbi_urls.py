1f90e5617fca1bedfde47942e1a79c9ae75ca6c5
Blake Sweeney <bsweeney@ebi.ac.uk>
2022-07-10 14:16:08 +0100

    Look up ids if needed
    It is possible to get a GCA id that is not a a complete id (missing the
    version). In this case we just assume they are using the latest. This is
    likely not to be true, but is reasonable enough as we will be on the
    latest that way. There really isn't an easy way to figure out what
    version is being used otherwise as well.

--- scripts/genome_download/bin/ncbi_urls.py
+++ scripts/genome_download/bin/ncbi_urls.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import re
 import json
 from pathlib import Path
 import typing as ty
@@ -20,6 +21,19 @@ def ftp_path(info: SqliteDict, accession: str) -> ty.Optional[str]:
     return f"{path}/{name}_genomic.fna.gz"
 
 
+def determine_latest(db: SqliteDict, id: str) -> str:
+    if id in db:
+        return id
+    possible = {}
+    pattern = re.compile(f"^{id}.(\d+)$")
+    for key in db.iterkeys():
+        if match := re.match(pattern, key):
+            index = int(match.group(1))
+            possible[index] = key
+    to_use = max(possible.keys())
+    return possible[to_use]
+
+
 @click.command()
 @click.argument('ncbi-file', type=click.Path())
 @click.argument('gca_file', type=click.File('r'))
@@ -31,7 +45,7 @@ def main(ncbi_file, gca_file, directory, url_file, ena_only):
         base = Path(directory)
         for line in gca_file:
             row = json.loads(line)
-            gca = row['accession']
+            gca = determine_latest(ncbi, row['accession'])
             save_path = base / f"{row['upi']}.fa.gz"
             url = ftp_path(ncbi, gca)
             if url is None:

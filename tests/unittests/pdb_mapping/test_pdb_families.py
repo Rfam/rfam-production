ec22fb48588659836b16f63a3701ebee55f14220
Emma Cooke <emma.cooke93@gmail.com>
2022-08-10 14:48:18 +0100

    fix mock open

--- tests/unittests/pdb_mapping/test_pdb_families.py
+++ tests/unittests/pdb_mapping/test_pdb_families.py
@@ -1,5 +1,5 @@
 import unittest
-from mock import patch
+from mock import patch, mock_open
 
 from pdb_mapping.pdb_families import list_new_families
 
@@ -15,7 +15,9 @@ class PdbTest(unittest.TestCase):
                               "(SELECT DISTINCT rfam_acc FROM pdb_full_region_old WHERE is_significant = 1);")
         conn = mock_connect.return_value
         cursor = conn.cursor.return_value
-        list_new_families()
+        with patch("__builtin__.open", mock_open(read_data="data")):
+            assert open("pdb_file.txt").read() == "data"
+            list_new_families()
         self.assertEqual(5, cursor.execute.call_count)
         cursor.execute.assert_called_with(new_families_query)
 
@@ -24,16 +26,20 @@ class PdbTest(unittest.TestCase):
         conn = mock_connect.return_value
         cursor = conn.cursor.return_value
         cursor.execute.side_effect = Exception
-        with self.assertRaises(Exception):
-            list_new_families()
+        with patch("__builtin__.open", mock_open(read_data="data")):
+            assert open("pdb_file.txt").read() == "data"
+            with self.assertRaises(Exception):
+                list_new_families()
 
     @patch('pdb_mapping.pdb_families.RfamDB.connect')
     def test_list_new_families_raises_exception(self, mock_connect):
         conn = mock_connect.return_value
         cursor = conn.cursor.return_value
         cursor.execute.side_effect = Exception
-        with self.assertRaises(Exception):
-            list_new_families()
+        with patch("__builtin__.open", mock_open(read_data="data")):
+            assert open("pdb_file.txt").read() == "data"
+            with self.assertRaises(Exception):
+                list_new_families()
 
 
 if __name__ == '__main__':

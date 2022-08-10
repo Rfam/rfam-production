b154e5f5554d87e189bbfb31dbcc482cb01bf839
Emma Cooke <emma.cooke93@gmail.com>
2022-08-10 14:30:48 +0100

    add unit tests

--- tests/unittests/release/test_scan_families.py
+++ tests/unittests/release/test_scan_families.py
@@ -28,3 +28,24 @@ class TestScans(unittest.TestCase):
         scan_families.checkout_and_search_family('RF00001', Mock())
         self.assertEqual(1, mock_submit.call_count)
         self.assertEqual(1, mock_checkout.call_count)
+
+    @patch('scan_families.subprocess.call')
+    def test_checkout_family(self, mock_subprocess):
+        scan_families.checkout_family('RF00001')
+        mock_subprocess.assert_called_with("rfco.pl RF00001", shell=True)
+
+    @patch('scan_families.subprocess.call')
+    def test_submit_new_rfsearch_job_rfmake_true(self, mock_subprocess):
+        rfmake_true_cmd = 'bsub -M 2000 -R "rusage[mem=2000]" -o /family/dir/path/auto_rfsearch.out -e ' \
+                          '/family/dir/path/auto_rfsearch.err -q bigmem "cd /family/dir/path && ' \
+                          'rfsearch.pl -scpu 0 -cnompi -q short && rfmake.pl -local"'
+        scan_families.submit_new_rfsearch_job('/family/dir/path', rfmake=True)
+        mock_subprocess.assert_called_with(rfmake_true_cmd, shell=True)
+
+    @patch('scan_families.subprocess.call')
+    def test_submit_new_rfsearch_job_rfmake_false(self, mock_subprocess):
+        rfmake_false_cmd = 'bsub -M 2000 -R "rusage[mem=2000]" -o /family/dir/path/auto_rfsearch.out -e ' \
+                          '/family/dir/path/auto_rfsearch.err -q bigmem "cd /family/dir/path && ' \
+                          'rfsearch.pl -scpu 0 -cnompi -q short"'
+        scan_families.submit_new_rfsearch_job('/family/dir/path')
+        mock_subprocess.assert_called_with(rfmake_false_cmd, shell=True)

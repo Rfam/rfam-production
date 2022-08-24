ddcbd9f5c672ba65aa3358f99415aa1db34ae842
Emma Cooke <emma.cooke93@gmail.com>
2022-08-24 16:08:51 +0100

    Update add_3d_output.py
    edit write to file

--- pdb_mapping/add_3d_output.py
+++ pdb_mapping/add_3d_output.py
@@ -8,21 +8,25 @@ def write_3d_output():
     Extract relevant info from the rfam-3d-seed-alignments script
     and write to a file to send to the Slack channel
     """
-    modified = "The following families have been updated with 3D information: "
-    added = "The following families have been newly added with 3D information: "
+    modified = "The following families have been updated with 3D information: \n"
+    added = "The following families have been newly added with 3D information: \n"
     with open(add_3d_git_output, "r") as output:
         contents = output.readlines()
         for line in contents:
-            if "M" in line:
-                modified += line
-            if "A" in line:
-                added +=line
+            if "data/output" in line:
+                line = line.replace("data/output/", "")
+                line = line.replace(".sto", "")
+                if "A" in line:
+                    line = line.replace("A", "")
+                    added += line
+                elif "M" in line:
+                    line = line.replace("M", "")
+                    modified += line
 
     today_date = str(datetime.date.today())
     pdb_txt = "{dir}/pdb_families_{date}.txt".format(dir=pdb_files, date=today_date)
 
     with open(pdb_txt, "a") as pdb_file:
-        pdb_file.write("\n")
         pdb_file.write(modified + "\n")
         pdb_file.write(added + "\n")
 

ad53159eca7ab68dce0ca767042c1e6357dd8fcf
Emma Cooke <emma.cooke93@gmail.com>
2023-09-18 10:51:59 +0100

    remove Entry class
    It was interfering with the format and wasn't really adding to this script

--- scripts/apicuron/extract_svn_info.py
+++ scripts/apicuron/extract_svn_info.py
@@ -10,14 +10,6 @@ import argparse
 import scripts.apicuron.conf as conf
 
 
-@dataclass
-class RfamActivity:
-    acitivity_term: str
-    timestamp: str
-    curator_orcid: ty.Optional[str]
-    entity_uri: str
-
-
 def get_author(revision: int, svn_url: str) -> str:
     """
     Run svnlook to get the author of the commit
@@ -42,7 +34,7 @@ def get_family(revision: int, svn_url: str):
     family_cmd = "svnlook dirs-changed -r {rev} {url}".format(rev=revision, url=svn_url)
     output = subprocess.check_output(family_cmd, shell=True, text=True)
     if match := re.search(r"(RF\d{5})", output):
-        return match.group(1)
+        return "https://rfam.org/family/" + match.group(1)
     return ""
 
 
@@ -57,8 +49,8 @@ def get_term(revision: int, svn_url: str) -> str:
     message_cmd = "svnlook log -r {rev} {url}".format(rev=revision, url=svn_url)
     output = subprocess.check_output(message_cmd, shell=True, text=True)
     activity_term = ""
-    for rfci_term, apicuron_term in conf.checkin_terms.items():
-        if rfci_term in output:
+    for ci_term, apicuron_term in conf.checkin_terms.items():
+        if ci_term in output:
             activity_term = apicuron_term
     return activity_term
 
@@ -75,6 +67,35 @@ def get_timestamp(revision: int, svn_url: str) -> str:
     output = subprocess.check_output(date_cmd, shell=True, text=True)
     timestamp = output[:19]
     return timestamp
+
+
+def write_report(args):
+    reports_current = []
+    reports_other = []
+    url = args.svn
+    for rev in range(args.start, args.end):
+        author = get_author(rev, url)
+        author_orcid = conf.curator_orcids.get(author, author)
+        entry = {
+            "activity_term": get_term(rev, url),
+            "timestamp": get_timestamp(rev, url),
+            "curator_orcid": author_orcid,
+            "entity_uri": get_family(rev, url),
+        }
+        if not any(entry.values()):
+            if author in conf.curator_orcids:
+                reports_current.append(entry)
+            else:
+                reports_other.append(entry)
+
+    today_date = str(datetime.date.today())
+    reports_file = "bulk_report_svn_" + today_date + ".json"
+    with open(reports_file, "w") as bulk_report:
+        reports = {"resource_id": "rfam", "reports": reports_current}
+        json.dump(reports, bulk_report, indent=4, sort_keys=True)
+    with open("others_" + reports_file, "w") as bulk_report:
+        reports = {"resource_id": "rfam", "reports": reports_other}
+        json.dump(reports, bulk_report, indent=4, sort_keys=True)
 
 
 def parse_args():
@@ -103,32 +124,7 @@ def main():
     extract_svn_info.py --end 16099 --start 15938 --svn /path/to/svn
     """
     args = parse_args()
-    reports_current = []
-    reports_other = []
-    url = args.svn
-    for rev in range(args.start, args.end):
-        author = get_author(rev, url)
-        author_orcid = conf.curator_orcids.get(author, author)
-        entry = RfamActivity(
-            get_term(rev, url),
-            get_timestamp(rev, url),
-            author_orcid,
-            "https://rfam.org/family/" + get_family(rev, url)
-        )
-
-        if author in conf.curator_orcids:
-            reports_current.append(entry)
-        else:
-            reports_other.append(entry)
-
-    today_date = str(datetime.date.today())
-    reports_file = "bulk_report_svn_" + today_date + ".json"
-    with open(reports_file, "w") as bulk_report:
-        reports = {"resource_id": "rfam", "reports": reports_current}
-        json.dump(reports, bulk_report, indent=4, sort_keys=True)
-    with open("others_" + reports_file, "w") as bulk_report:
-        reports = {"resource_id": "rfam", "reports": reports_other}
-        json.dump(reports, bulk_report, indent=4, sort_keys=True)
+    write_report(args)
 
 
 if __name__ == "__main__":
